a1d5a679647f1b551555396126c29356e6b736ee
Tiranat Kamklai <tiranat@gmail.com>
2021-10-12 16:41:11 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    if (principal <=0)
+    {
+        alert ("Enter a positive number");
+        return 0;
+    }
+    document.getElementById("result").innerHTML = "If you deposit: <mark>"+principal+"</mark><br>At an interest rate of :<mark>"+rate+"</mark><br>You will receive an amount of :<mark>"+interest+"</mark>,<br>In the year :<mark>"+year+"</mark><br>";
 }
+
+    function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+   
+    
+
         
